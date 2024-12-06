c076a108c431f7775056e1c5543c269d67dc0a89
paco7828 <koszegipatrik888@gmail.com>
2024-12-07 00:11:21 +0100

    Added minimalistic design.

--- index.php
+++ index.php
@@ -5,16 +5,18 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Log in</title>
+    <link rel="stylesheet" href="./styles/style.css">
+    <link rel="stylesheet" href="./styles/index.css">
 </head>
 
 <body>
-    <div id="container">
+    <div id="container" class="container-fluid text-center">
         <div id="constantCredentials">
-            <h1>Log in with your credentials</h1>
+            <h1>Credentials</h1>
             <form action="./userPage.php" method="POST">
-                <input autocomplete="off" type="text" placeholder="Server name..." name="servernameInput"><br>
-                <input autocomplete="off" type="text" placeholder="Username..." name="usernameInput"><br>
-                <input autocomplete="off" type="text" placeholder="Password..." name="passwordInput"><br>
+                <input autocomplete="off" type="text" placeholder="Server name..." name="servernameInput" class="form-control-md"><br>
+                <input autocomplete="off" type="text" placeholder="Username..." name="usernameInput" class="form-control-md"><br>
+                <input autocomplete="off" type="text" placeholder="Password..." name="passwordInput" class="form-control-md"><br>
                 <input type="submit" value="Login">
             </form>
         </div>
